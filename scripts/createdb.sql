29e193bb14da0aad325ae377d3b9c4520147b7c1
Jason Sayre <ejsayre@gmail.com>
2015-07-14 11:00:20 -0400

    Forward thinking in the event PASSWORD_DEFAULT changes to something larger

--- scripts/createdb.sql
+++ scripts/createdb.sql
@@ -4,9 +4,8 @@ create table if not exists users(
   username varchar(32) not null,
   unique key username(username),
   email varchar(254),
-  password char(64),
-  salt char(64)
+  password char(256)
 )default character set=utf8;
-insert into users (username, email, password, salt) values ('Jason', 'ejsayre@example.com', 'pass', 'salt');
-insert into users (username, email, password, salt) values ('Nick', 'nick@example.com', 'password1234', 'salt');
-# insert into users (username, email, password, salt) values ('Gabe', 'gabe@example.com', 'pass321', 'salt');
+insert into users (username, email, password, salt) values ('Jason', 'ejsayre@example.com', 'pass');
+insert into users (username, email, password, salt) values ('Nick', 'nick@example.com', 'password1234');
+# insert into users (username, email, password, salt) values ('Gabe', 'gabe@example.com', 'pass321');
